a5357813682ce11f1f1daf5aee52244c5558d351
xdo13 <xdo13@naver.com>
2024-10-04 17:54:16 +0900

    특정 범위를 벗어나는 경우 프로그램이 종료하는 구문 추가

--- src/prac1.java
+++ src/prac1.java
@@ -6,10 +6,16 @@ public class prac1 {
         //Scanner 클래스를 이용해 키보드로 입력받는 코드
         Scanner stdIn = new Scanner(System.in);
         //4개의 과목을 각각 키보드로 입력받는 구문 생성
-        System.out.println("국어 점수를 입력하세요 : "); int a = stdIn.nextInt();
-        System.out.println("수학 점수를 입력하세요 : "); int b = stdIn.nextInt();
-        System.out.println("사회 점수를 입력하세요 : "); int c = stdIn.nextInt();
-        System.out.println("과학 점수를 입력하세요 : "); int d = stdIn.nextInt();
+        System.out.print("국어 점수를 입력하세요 : "); int a = stdIn.nextInt();
+        System.out.print("수학 점수를 입력하세요 : "); int b = stdIn.nextInt();
+        System.out.print("사회 점수를 입력하세요 : "); int c = stdIn.nextInt();
+        System.out.print("과학 점수를 입력하세요 : "); int d = stdIn.nextInt();
+        
+        //0~100이 아닌 수가 입력되었을때 평균을 구하지않고 즉시 프로그램을 종료
+        if ((a<0 || a >100) | (b<0 || b >100) | (c<0 || c >100) | (d<0 || d >100))
+            System.out.println("잘못된 입력입니다. 다시 입력하세요");
+            System.exit(0);
+
 
         //과목합계를 입력받는 sum을 double의 자료형으로 저장
         double sum = a+b+c+d;
